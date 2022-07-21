0dde9724cc47ecc0911cef030000e8a3ee34d5d9
Milk Steak <a.khatttab97@gmail.com>
2022-07-21 09:24:23 +0200

    Populate display with input values

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let DEFAULT_DISPLAY = '0'
+let num = ''
+
+let active_num = document.getElementById('active-num');
+let numButtons = Array.from(document.querySelectorAll(`[data-number]`));
+numButtons.forEach(btn => btn.addEventListener('click', primeDisplay));
 
 
 
+active_num.textContent = DEFAULT_DISPLAY;
 
-
+function primeDisplay(e){
+	console.log(e.target.getAttribute('data-number'))
+	num += e.target.getAttribute('data-number')
+	active_num.textContent = num;
+}
 
 
 
